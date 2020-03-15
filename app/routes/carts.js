8ff1ca22afa6862bf7f107e8c82c0063cc96de71
Sara-Kuddah <sara-kuddah@hotmail.com>
2020-03-15 11:39:27 +0300

    create DELETE by ID function for cart

--- app/routes/carts.js
+++ app/routes/carts.js
@@ -29,6 +29,17 @@ router.post('/api/cart',(req,res)=>{
     });
 });
 
+// {
+// 	"cart":{
+// 		"itemId" : "66" ,
+//     "totalprice" : 400 ,
+//     "name": "Ar",
+//     "price": 400, 
+//     "amount" : 1 ,
+//     "img": "String"
+// 	} 
+//   }
+
 /**
  * Action:        SHOW
  * Method:        GET
@@ -49,7 +60,38 @@ router.get("/api/cart", (req, res) => {
   
 
 
-
+/**
+ * Action:       DESTROY
+ * Method:       DELETE
+ * URI:          /api/cart/5d66b8b68b
+ * DEscriptin:   Delete An Articale  by Article ID
+ */
+router.delete('/api/cart/:id',(req,res)=>{
+  Cart.findById(req.params.id)
+  .then((cart)=>{
+    if(cart){ 
+        //Pass the resulte og Mongoose`s .delete method to the next then
+        return cart.remove();
+    }else{
+        //if we couldnt fine a document with the matching ID
+        res.status(404).json({
+            error:{
+                name:'DocumentNot FoundError',
+                message:'The provided ID Doesn\`t match any documents'
+            }
+        });
+    }
+ })
+    .then(()=>{
+ //If the deletion succeeded, return 204 and no JSON
+  res.status(204).end();
+  })
+ 
+  .catch((error)=>{
+     res.status(500).json({error:error});
+  });
+ });
+ 
 
 //Export the Router so we can use it in server.js file
 module.exports=router;
